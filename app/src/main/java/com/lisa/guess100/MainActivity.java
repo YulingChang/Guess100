236549282df3687692b9ba51f3720cc45619d3cd
Yu Ling <aa0932299020@gmail.com>
2017-11-14 10:17:47 +0800

    findViews

--- app/src/main/java/com/lisa/guess100/MainActivity.java
+++ app/src/main/java/com/lisa/guess100/MainActivity.java
@@ -1,7 +1,11 @@
 package com.lisa.guess100;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        Button bSend = (Button) findViewById(R.id.send);
+        bSend.setOnClickListener(new View.OnClickListener(){
+
+
+
+
+            @Override
+            public void onClick(View view) {
+
+            }
+
+            public void guess (View view) {
+                EditText edsecret = (EditText) findViewById(R.id.secret);
+                EditText edinfo = (EditText) findViewById(R.id.info);
+
+
+            }}}}
